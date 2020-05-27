8f239e81c5f8619cf60690d486ab99c6e6332da2
子健想发财 <45197869+wuzijian4@users.noreply.github.com>
2020-05-27 18:26:04 +0800

    Update 176InternetBandwidth.cpp

--- Answersheet/176InternetBandwidth.cpp
+++ Answersheet/176InternetBandwidth.cpp
@@ -1,52 +1,66 @@
-#include <iostream>
-#define _USE_MATH_DEFINES
-#include <cmath>
-#include <math.h>
-#include <iomanip>
-#include <algorithm>
-
+#include <cstdio>
+#include <cstring>
+#include <queue>
 using namespace std;
+#define INF 0x3f3f3f3f
+#define N 110
+int f[N][N],c[N][N],a[N],p[N];
+//a数组是一个递推数组,p相当于记录路径
+int n,m,s,t,flow;
+queue <int> q;
 
-struct circle
+void EK()
 {
-	int x, y, r;
-}c[300];
+    flow=0;
+    while(!q.empty()) q.pop();
+    while(1)
+    {
+        memset(a,0,sizeof(a));
+        a[s]=INF;
+        q.push(s);
+        while(!q.empty())
+        {
+            int u;
+            u=q.front(); q.pop();
+            for(int v=1; v<=n; v++)  //枚举所有点v; <u,v>
+                if(!a[v] && c[u][v]>f[u][v])
+                {
+                    p[v]=u;  q.push(v);
+                    a[v]=a[u]<c[u][v]-f[u][v] ? a[u] : c[u][v]-f[u][v];
+                    //递推
+                }
+        }
 
-
-
-int main() {
-	int n;
-	do
-	{
-		cin >> n;
-		if (n == 0)
-			break;
-		for (int i = 1; i <= n; i++)
-			cin >> c[i].x >> c[i].y >> c[i].r;
-		double max = 0;
-
-		for (double i = 0; i <= 360; i += 0.25)
-		{
-			double u = 500 * cos(i*M_PI / 180);
-			double v = 500 * sin(i*M_PI / 180);
-			double total = 0;
-			for (int j = 1; j <= n; j++)
-			{
-				double length = (u*c[j].x + v * c[j].y) / (sqrt(pow(u, 2) + pow(v, 2)));
-				if (length > 0) {
-					// cos(theta) must larger than 0, theta cannot be larger than 90 degree
-					double perpendicular = pow(c[j].x, 2) + pow(c[j].y, 2) - pow(length, 2);
-					if (perpendicular < pow(c[j].r, 2)) {
-						// have intersections
-						double newLength = 2 * sqrt(pow(c[j].r, 2) - perpendicular);
-						total += newLength;
-					}
-				}
-			}
-			if (total > max)
-				max = total;
-		}
-		cout << setprecision(3) << fixed << max << endl;
-	} while (true);
-	return 0;
+        if(!a[t]) break;
+        for(int u=t; u!=s; u=p[u])
+        {
+            f[p[u]][u]+=a[t];
+            f[u][p[u]]-=a[t];
+        }
+        flow+=a[t];
+    }
+    return ;
+}
+int main()
+{
+    int T=0;
+    while(scanf("%d",&n)!=EOF && n)
+    {
+        scanf("%d%d%d",&s,&t,&m);
+        memset(p,0,sizeof(p));
+        memset(f,0,sizeof(f));
+        memset(c,0,sizeof(c));
+        for(int i=1 ;i<=m; i++)
+        {
+            int u,v,w;
+            scanf("%d%d%d",&u,&v,&w);
+            c[u][v]+=w;          //注意建图
+            c[v][u]=c[u][v];
+        }
+        EK();
+        T++;
+        printf("Network %d\n",T);
+        printf("The bandwidth is %d.\n\n",flow);
+    }
+    return 0;
 }
