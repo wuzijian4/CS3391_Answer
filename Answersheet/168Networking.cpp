e7c3ea3ac1c016f345d41237343102d52d0c86d1
子健想发财 <45197869+wuzijian4@users.noreply.github.com>
2020-05-27 18:18:56 +0800

    Update 168Networking.cpp

--- Answersheet/168Networking.cpp
+++ Answersheet/168Networking.cpp
@@ -1,52 +1,83 @@
 #include <iostream>
-#define _USE_MATH_DEFINES
-#include <cmath>
-#include <math.h>
-#include <iomanip>
-#include <algorithm>
-
+#include <memory.h>
+#include <stdlib.h>
 using namespace std;
 
-struct circle
-{
-	int x, y, r;
-}c[300];
+int p, r;
+int parent[51];
+// the maximum of nodes is 50
 
+struct edge
+{
+	int n1, n2;
+	int len;
+};
+
+int cmp(const void *a, const void *b) {
+	edge *x = (edge*)a;
+	edge *y = (edge*)b;
+	return x->len - y->len;
+}
+
+int find(int e) {
+	if (parent[e] < 0)
+		return e;      // find the parent node
+	else
+		return parent[e] = find(parent[e]); // path compression, point to the top most element
+}
+
+bool Union(int e1, int e2, int len) {
+	int root1 = find(e1);
+	int root2 = find(e2);
+
+	//并查集， 将不在同一个set的node合并在一起
+	if (root1 == root2)
+		return false; // two nodes belong to the same set, 
+	//cycle formed, return false;
+
+	if (parent[root1] < parent[root2]) {
+		parent[root1] += parent[root2];
+		parent[root2] = root1;
+	}
+	else
+	{
+		parent[root2] += parent[root1];
+		parent[root1] = root2;
+	}
+	return true;
+}
 
 
 int main() {
-	int n;
 	do
 	{
-		cin >> n;
-		if (n == 0)
+		cin >> p >> r;
+		if (p == 0)
 			break;
-		for (int i = 1; i <= n; i++)
-			cin >> c[i].x >> c[i].y >> c[i].r;
-		double max = 0;
-
-		for (double i = 0; i <= 360; i += 0.25)
-		{
-			double u = 500 * cos(i*M_PI / 180);
-			double v = 500 * sin(i*M_PI / 180);
-			double total = 0;
-			for (int j = 1; j <= n; j++)
-			{
-				double length = (u*c[j].x + v * c[j].y) / (sqrt(pow(u, 2) + pow(v, 2)));
-				if (length > 0) {
-					// cos(theta) must larger than 0, theta cannot be larger than 90 degree
-					double perpendicular = pow(c[j].x, 2) + pow(c[j].y, 2) - pow(length, 2);
-					if (perpendicular < pow(c[j].r, 2)) {
-						// have intersections
-						double newLength = 2 * sqrt(pow(c[j].r, 2) - perpendicular);
-						total += newLength;
-					}
-				}
-			}
-			if (total > max)
-				max = total;
+		if (r == 0) {
+			cout << 0 << endl;
+			continue;
 		}
-		cout << setprecision(3) << fixed << max << endl;
+
+		edge *edges = new edge[r+1];
+		memset(parent, -1, sizeof(parent));
+		for (int i = 0; i < r; i++)
+			cin >> edges[i].n1 >> edges[i].n2 >> edges[i].len;
+
+		qsort(edges, r, sizeof(edge), cmp);
+		int numOfEdges = 0;
+		int cable = 0;
+		for (int i = 0; i < r; i++)
+		{
+			if (Union(edges[i].n1, edges[i].n2, edges[i].len))
+			{
+				cable += edges[i].len;
+				numOfEdges++;
+			}
+			if (numOfEdges == p - 1)
+				break;
+		}
+		cout << cable << endl;
 	} while (true);
 	return 0;
 }
