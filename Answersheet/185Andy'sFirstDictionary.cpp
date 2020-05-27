4a91871b2ed091c1d6ba56abede0d0c54e702849
子健想发财 <45197869+wuzijian4@users.noreply.github.com>
2020-05-27 18:32:49 +0800

    Update 185Andy'sFirstDictionary.cpp

--- Answersheet/185Andy'sFirstDictionary.cpp
+++ Answersheet/185Andy'sFirstDictionary.cpp
@@ -1,52 +1,32 @@
 #include <iostream>
-#define _USE_MATH_DEFINES
-#include <cmath>
-#include <math.h>
-#include <iomanip>
-#include <algorithm>
-
+#include <string>
+#include <set>
 using namespace std;
-
-struct circle
-{
-	int x, y, r;
-}c[300];
-
 
 
 int main() {
-	int n;
-	do
+	string input;
+	set<string>s; //less 函数, 默认升序, 如果使用结构体，要重载运算符
+	s.clear();
+	while (getline(cin, input))
 	{
-		cin >> n;
-		if (n == 0)
-			break;
-		for (int i = 1; i <= n; i++)
-			cin >> c[i].x >> c[i].y >> c[i].r;
-		double max = 0;
-
-		for (double i = 0; i <= 360; i += 0.25)
+		string str = "";
+		for (int i = 0; i <= input.length(); i++)
 		{
-			double u = 500 * cos(i*M_PI / 180);
-			double v = 500 * sin(i*M_PI / 180);
-			double total = 0;
-			for (int j = 1; j <= n; j++)
-			{
-				double length = (u*c[j].x + v * c[j].y) / (sqrt(pow(u, 2) + pow(v, 2)));
-				if (length > 0) {
-					// cos(theta) must larger than 0, theta cannot be larger than 90 degree
-					double perpendicular = pow(c[j].x, 2) + pow(c[j].y, 2) - pow(length, 2);
-					if (perpendicular < pow(c[j].r, 2)) {
-						// have intersections
-						double newLength = 2 * sqrt(pow(c[j].r, 2) - perpendicular);
-						total += newLength;
-					}
-				}
+			if (i<input.length() && isalpha(input[i])) {
+				char tmp = tolower(input[i]);
+				str += tmp;
 			}
-			if (total > max)
-				max = total;
+			else if(str.length() > 0)
+			{
+				s.insert(str);
+				str = "";
+			}
 		}
-		cout << setprecision(3) << fixed << max << endl;
-	} while (true);
+	}
+	// 创建一个迭代器
+	set<string>::iterator it;
+	for (it = s.begin(); it != s.end(); it++)
+		cout << *it << endl;
 	return 0;
 }
