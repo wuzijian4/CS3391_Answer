597c91d7cf92b93d13b6b983b79ed5354973fb49
子健想发财 <45197869+wuzijian4@users.noreply.github.com>
2020-05-27 18:26:45 +0800

    Update 178TheProblemWithProblemSetter.cpp

--- Answersheet/178TheProblemWithProblemSetter.cpp
+++ Answersheet/178TheProblemWithProblemSetter.cpp
@@ -1,52 +1,90 @@
 #include <iostream>
-#define _USE_MATH_DEFINES
-#include <cmath>
-#include <math.h>
-#include <iomanip>
+#include <queue>
+#include <climits>
 #include <algorithm>
-
+#include <memory.h>
 using namespace std;
 
-struct circle
-{
-	int x, y, r;
-}c[300];
 
+#define INF INT_MAX
+#define N 1100
+int f[N][N], c[N][N], a[N], p[N];
+//a数组是一个递推数组,p相当于记录路径
+int n, m, s, t, flow;
+queue <int> q;
+
+void EK()
+{
+	flow = 0;
+	while (!q.empty()) q.pop();
+	while (1)
+	{
+		memset(a, 0, sizeof(a));
+		a[s] = INF;
+		q.push(s);
+		while (!q.empty())
+		{
+			int u;
+			u = q.front(); q.pop();
+			for (int v = 1; v <= t; v++)  //枚举所有点v; <u,v>
+				if (!a[v] && c[u][v] > f[u][v])
+				{
+					p[v] = u;  q.push(v);
+					a[v] = a[u] < c[u][v] - f[u][v] ? a[u] : c[u][v] - f[u][v];
+					//递推
+				}
+		}
+
+		if (!a[t]) break;
+		for (int u = t; u != s; u = p[u])
+		{
+			f[p[u]][u] += a[t];
+			f[u][p[u]] -= a[t];
+		}
+		flow += a[t];
+	}
+	return;
+}
 
 
 int main() {
-	int n;
 	do
 	{
-		cin >> n;
-		if (n == 0)
+		cin >> n >> m;
+		if (n == 0 && m == 0)
 			break;
-		for (int i = 1; i <= n; i++)
-			cin >> c[i].x >> c[i].y >> c[i].r;
-		double max = 0;
+		memset(f, 0, sizeof(f));
+		memset(c, 0, sizeof(c));
+		memset(p, 0, sizeof(p));
+		s = 0; t = n + m + 1;
 
-		for (double i = 0; i <= 360; i += 0.25)
-		{
-			double u = 500 * cos(i*M_PI / 180);
-			double v = 500 * sin(i*M_PI / 180);
-			double total = 0;
-			for (int j = 1; j <= n; j++)
-			{
-				double length = (u*c[j].x + v * c[j].y) / (sqrt(pow(u, 2) + pow(v, 2)));
-				if (length > 0) {
-					// cos(theta) must larger than 0, theta cannot be larger than 90 degree
-					double perpendicular = pow(c[j].x, 2) + pow(c[j].y, 2) - pow(length, 2);
-					if (perpendicular < pow(c[j].r, 2)) {
-						// have intersections
-						double newLength = 2 * sqrt(pow(c[j].r, 2) - perpendicular);
-						total += newLength;
-					}
-				}
-			}
-			if (total > max)
-				max = total;
+		int total = 0;
+		for (int i = 1; i <= n; i++){
+			cin >> c[s][i];
+			total += c[s][i];
 		}
-		cout << setprecision(3) << fixed << max << endl;
+		for (size_t i = 1; i <= m; i++)
+		{
+			int numOfCater;
+			cin >> numOfCater;
+			while (numOfCater--) {
+				int cater;
+				cin >> cater;
+				c[cater][i+n] = 1;
+			}
+			c[i + n][t] = 1;
+		}
+
+
+
+		EK();
+		
+		if (flow == total)
+			cout << 1 << endl;
+		else
+			cout << 0 << endl;
+
 	} while (true);
+
 	return 0;
 }
